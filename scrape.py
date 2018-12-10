0ea75b036a8cdafdaba44b805271b9c6b0736436
JinyuanL <35589730+JinyuanL@users.noreply.github.com>
2018-12-10 14:59:19 -0600

    Update scrape.py
    for web scraping the dataset of names

--- scrape.py
+++ scrape.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
 import bs4 as bs
 from bs4 import BeautifulSoup
 import urllib.request
 import pandas as pd
-
-
-# In[2]:
-
 
 data = {
     'name' : [],
     'meaning' : [],
     'gender' : []
 }
-
-
-# In[9]:
-
 
 for page in range(1, 214):  # English names has 213 pages 
     sauce = urllib.request.urlopen('https://www.momjunction.com/baby-names/english/page/' + str(page)).read()
@@ -36,15 +24,8 @@ for page in range(1, 214):  # English names has 213 pages
             data['meaning'].append(row[1])
             data['gender'].append(row[2])
 
-
-# In[3]:
-
-
 file = pd.DataFrame(data)
 file.to_csv("english_name.csv")
-
-
-# In[11]:
 
 
 data2 = {
@@ -52,6 +33,7 @@ data2 = {
     'meaning' : [],
     'gender' : []
 }
+
 for page in range(1, 19):  # African names has 18 pages 
     sauce = urllib.request.urlopen('https://www.momjunction.com/baby-names/african/page/' + str(page)).read()
     soup = bs.BeautifulSoup(sauce, 'lxml')
@@ -65,15 +47,8 @@ for page in range(1, 19):  # African names has 18 pages
             data2['meaning'].append(row[1])
             data2['gender'].append(row[2])
 
-
-# In[12]:
-
-
 file2 = pd.DataFrame(data2)
 file2.to_csv("african_name.csv")
-
-
-# In[14]:
 
 
 data3 = {
@@ -96,4 +71,3 @@ for page in range(1, 65):  # Hebrew names has 64 pages
             
 file3 = pd.DataFrame(data3)
 file3.to_csv("hebrew_name.csv")
-
